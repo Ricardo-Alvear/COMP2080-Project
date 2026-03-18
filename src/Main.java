8d815490bff0f668fcb3abbf10853cf87bc66bbf
ricardo <ricardo.alvear.personal@gmail.com>
2026-03-18 12:55:33 -0400

    upload of the files

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        // Name of members and ids:
+
+        // Sayuni will do this:
+//        2) If the user selects 1 player (versus minimax AI)
+//        a. The Human player should be asked their name
+//        b. The player must be prompted to choose their symbol (‘X’ or ‘O’)
+//        c. The player with symbol ‘X’ should go first (computer or human player).
+//                d. The present board state will be displayed and:
+//        1. If the computer is to play, it will now show the board updated with the
+//        computer’s symbol.
+//        2. If it is the player’s turn, the player will be prompted for a row and column
+//        location to play.
+//                e. The board will then be checked for a winning state or draw and if it is not in a winning
+//        state or draw(tie), go to step d otherwise display the winner or declare a draw (tie).
+
+
+
+
+
+
+
+        // RICARDO WILL DO THIS
+
+        // 3) If the user selects 2 player (2 Human players)
+        //a. Each human player must be prompted to choose their name and symbol (‘X’ or ‘O’)
+        //b. An empty board will then be displayed.
+        //c. The next player to move will be prompted for a row and column location to play.
+        //d. The present board state will be displayed showing their updated play.
+        //e. The board will then be checked for a winning state or draw and if it is not in a winning
+        //state or draw, go to step c otherwise display the winner or declare a draw
+
+    }
+}
+
+class Test{
+
+}
